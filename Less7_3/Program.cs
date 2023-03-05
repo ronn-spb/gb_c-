10d8baf84b9e6632b807e0ac50da84852c9e0014
Ronnspb <shooter12@mail.ru>
2023-03-05 13:11:40 +0300

    less7 complite

---
+++ Less7_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] array = new int[,] { { 1, 4, 7 }, { 3, 8, 4 }, { 8, 2, 3 } };
+
+double sum1 = 0;
+double sum2 = 0;
+double sum3 = 0;
+double avg1, avg2, avg3;
+for (int i = 0; i < 3; i++)
+{
+    sum1 += array[i, 0];
+    sum2 += array[i, 1];
+    sum3 += array[i, 2];
+}
+avg1 = sum1 / 3;
+avg2 = sum2 / 3;
+avg3 = sum3 / 3;
+Console.WriteLine($"Среднее арифметическое элементов 1 столбца = {avg1}");
+Console.WriteLine($"Среднее арифметическое элементов 2 столбца = {avg2}");
+Console.WriteLine($"Среднее арифметическое элементов 3 столбца = {avg3}");
